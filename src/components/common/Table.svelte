1ba09f19d7f40f9252435196bda2cd515520963f
vanviethieuanh@gmail.com <vanviethieuanh@gmail.com>
2022-07-06 00:02:28 +0700

    Common component: Table

---
+++ src/components/common/Table.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let columnsMapper: { title: string; value: Function }[] = []
+    export let data: Object[] = []
+</script>
+
+<table>
+    <thead>
+        <tr>
+            {#each columnsMapper as column}
+                <th>{column.title}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each data as item}
+            <tr>
+                {#each columnsMapper as column}
+                    <td>{column.value(item)}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
