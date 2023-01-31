622c4807b9156fb775d6134c5a6de189d52568b6
ncdmz\vvha <vvha@netcompany.cloud>
2023-01-31 14:10:34 +0700

    Style

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,49 @@
 <script lang="ts">
     // Import components
-    import Table from '@components/common/Table.svelte'
+    import Table from "@components/common/Table.svelte"
 
     // Import logics
-    import { Process } from '@models/Process'
-    import * as Scheduler from '@logic/Scheduler'
+    import { Process } from "@models/Process"
+    import * as Scheduler from "@logic/Scheduler"
 
     // Import store variables
-    import { processes } from '@store'
-    import Grantt from '@components/Grantt.svelte';
+    import { processes } from "@store"
+    import Grantt from "@components/Grantt.svelte"
 
     let inputProcesses = [
-        new Process('A', 0, 5, 1),
-        new Process('B', 1, 4, 2),
-        new Process('C', 2, 3, 3),
-        new Process('D', 3, 2, 4),
-        new Process('E', 4, 1, null),
+        new Process("A", 0, 5, 1),
+        new Process("B", 1, 4, 2),
+        new Process("C", 2, 3, 3),
+        new Process("D", 3, 2, 4),
+        new Process("E", 4, 1, null),
     ]
 
     // define column configs
     const inputColumnsMapper = [
         {
-            title: 'Name',
+            title: "Name",
             getter: (v) => v.name,
             setter: (process, value) => {
                 process.name = value
             },
         },
         {
-            title: 'Arrival Time',
+            title: "Arrival Time",
             getter: (v) => v.arrivalTime,
             setter: (process, value) => {
                 process.arrivalTime = value
             },
         },
         {
-            title: 'Burst Time',
+            title: "Burst Time",
             getter: (v) => v.burstTime,
             setter: (process, value) => {
                 process.burstTime = value
             },
         },
         {
-            title: 'Priority',
-            getter: (v) => (v.priority ? v.priority : '0'),
+            title: "Priority",
+            getter: (v) => (v.priority ? v.priority : "0"),
             setter: (process, value) => {
                 process.priority = value
             },
@@ -52,38 +52,38 @@
 
     const resultColumnsMapper = [
         {
-            title: 'Name',
+            title: "Name",
             getter: (v) => v.name,
             setter: (obj, value) => {
-                throw 'Not Implemented'
+                throw "Not Implemented"
             },
         },
         {
-            title: 'Finish Time',
+            title: "Finish Time",
             getter: (v) => v.finishedTime,
             setter: (obj, value) => {
-                throw 'Not Implemented'
+                throw "Not Implemented"
             },
         },
         {
-            title: 'Wait Time',
+            title: "Wait Time",
             getter: (v) => v.waitTime,
             setter: (obj, value) => {
-                throw 'Not Implemented'
+                throw "Not Implemented"
             },
         },
         {
-            title: 'Turnaround Time',
+            title: "Turnaround Time",
             getter: (v) => v.turnaroundTime,
             setter: (obj, value) => {
-                throw 'Not Implemented'
+                throw "Not Implemented"
             },
         },
         {
-            title: 'Response Time',
+            title: "Response Time",
             getter: (v) => v.responseTime,
             setter: (obj, value) => {
-                throw 'Not Implemented'
+                throw "Not Implemented"
             },
         },
     ]
@@ -98,21 +98,20 @@
     $: averageTurnaroundTime =
         result.reduce((acc, v) => acc + v.turnaroundTime, 0) / result.length
 
-    $: runs = result
-        .map(process => process.toGranttPeriods())
-        .flat()
+    $: runs = result.map((process) => process.toGranttPeriods()).flat()
 
     function updateInput(event) {
         inputProcesses = event.detail.data
     }
 </script>
+
 <main>
     <div class="grid-displayer">
         <div class="table-container">
             <Table
                 bind:data={inputProcesses}
                 columnsMapper={inputColumnsMapper}
-                columnInputTypes={['text', 'number', 'number', 'number']}
+                columnInputTypes={["text", "number", "number", "number"]}
                 editable={true}
                 copyable={false}
                 entityName="process"
@@ -130,18 +129,19 @@
         </div>
     </div>
     <div>
-        <Grantt runs={runs}></Grantt>
+        <Grantt {runs} />
     </div>
 </main>
 
-<style>
+<style global lang="postcss">
     @tailwind base;
     @tailwind components;
     @tailwind utilities;
 
-    @import 'https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css';
+    @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+
     main {
-        font-family: 'Montserrat', sans-serif;
+        font-family: "Montserrat", sans-serif;
 
         text-align: center;
         padding: 1em;
--- src/components/Grantt.svelte
+++ src/components/Grantt.svelte
@@ -1,47 +1,53 @@
 <script lang="ts">
-    import type { GranttPeriod } from '@models/GranttPeriod'
+    import type { GranttPeriod } from "@models/GranttPeriod"
 
     export let runs: GranttPeriod[] = []
 
-    $: cols = Math.max(...runs.map(run => run.periodEnd))
-    $: processesName = [...new Set(runs.map(run => run.periodName))]
+    $: cols = Math.max(...runs.map((run) => run.periodEnd))
+    $: processesName = [...new Set(runs.map((run) => run.periodName))]
 </script>
 
-<div>
-    {#each runs as run}
-        <p>{run.periodName}-{run.periodStart}-{run.periodEnd}</p>
+<table>
+    {#each processesName as name}
+        <tr>
+            <td>{name}</td>
+            <td>
+                <div class="bar" style={`--cols: ${cols}`}>
+                    {#each runs as run}
+                        {#if run.periodName === name}
+                            <div
+                                class="grantt"
+                                style={`--start: ${
+                                    run.periodStart + 1
+                                };--end: ${run.periodEnd + 1}`}
+                            />
+                        {/if}
+                    {/each}
+                </div>
+            </td>
+        </tr>
     {/each}
-    {cols}
-    <table>
-        {#each processesName as name}
-            <tr>
-                <td>{name}</td>
-                <td>
-                    <div class="bar" style="{`--cols: ${cols}`}">
-                        {#each runs as run}
-                            {#if run.periodName === name}
-                                <div class="grantt" style="{`--start: ${run.periodStart+1};--end: ${run.periodEnd+1}`}">
-
-                                </div>
-                            {/if}
-                        {/each}
-                    </div>
-                </td>
-            </tr>
-        {/each}
-    </table>
-</div>
+</table>
 
 <style>
-    .bar{
+    td:first-child {
+        padding-right: min(1rem, 20px);
+    }
+
+    td:last-child{
+        background-color: #efefef;
+    }
+
+    .bar {
         display: grid;
-        grid-template-columns: repeat(var(--cols), 1fr 50px);
+        gap: 1px;
+        grid-template-columns: repeat(var(--cols), 20px);
         height: 100%;
     }
 
-    .grantt{
-        background-color: black;
-        height: 30px;
+    .grantt {
+        background-color: #222222;
+        height: 20px;
         grid-column: var(--start) / var(--end);
     }
 </style>
