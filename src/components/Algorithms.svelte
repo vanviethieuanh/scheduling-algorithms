f402195e3ec9039d36274ec12b89d8aa374eab5e
ncdmz\vvha <vvha@netcompany.cloud>
2023-02-02 19:19:38 +0700

    Preemptive option, quantumn box

--- src/components/Algorithms.svelte
+++ src/components/Algorithms.svelte
@@ -47,6 +47,9 @@
     ]
 
     let selectedIndex: number = 0
+    let quantumnTime: number = 0
+    let isPreemptive: boolean = false
+
     const dispatch = createEventDispatcher()
 </script>
 
@@ -69,9 +72,33 @@
             </li>
         {/each}
     </ul>
+    <div class="divider" />
+    <div class="settings">
+        <input
+            id="quantumn-time"
+            placeholder="Quantum Time"
+            bind:value={quantumnTime}
+        />
+        <div class="switch-button">
+            <label class="switch">
+                <input
+                    type="checkbox"
+                    bind:checked={isPreemptive}
+                />
+                <span class="slider round" />
+            </label>
+            <div>Preemptive</div>
+        </div>
+    </div>
 </div>
 
 <style lang="scss">
+    .algo {
+        display: grid;
+        grid-template-columns: 1fr auto auto;
+        gap: 1rem;
+    }
+
     ul {
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
@@ -104,6 +131,115 @@
         &:hover {
             background-color: rgba($color: #fff, $alpha: 0.2);
             border: rgba($color: #fff, $alpha: 0.5) 1px solid;
+        }
+    }
+
+    .divider {
+        width: 1px;
+        height: 100%;
+        background: linear-gradient(
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.1) 20%,
+            rgba(255, 255, 255, 0.15) 50%,
+            rgba(255, 255, 255, 0.1) 80%,
+            rgba(255, 255, 255, 0) 100%
+        );
+    }
+
+    .settings {
+        display: flex;
+        flex-direction: column;
+
+        #quantumn-time {
+            width: 100%;
+
+            padding: 0.5rem 1rem;
+            color: white;
+
+            background-color: rgba($color: #fff, $alpha: 0.1);
+            border: rgba($color: #fff, $alpha: 0.1) 1px solid;
+
+            &:hover {
+                background-color: rgba($color: #fff, $alpha: 0.2);
+                border: rgba($color: #fff, $alpha: 0.5) 1px solid;
+            }
+
+            &:focus {
+                background-color: rgba($color: #fff, $alpha: 0.2);
+                border: rgba($color: #fff, $alpha: 0.5) 1px solid;
+                outline: none;
+            }
+        }
+    }
+
+    .switch-button {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.7rem;
+        color: white;
+    }
+
+    .switch {
+        $height: 24px;
+        $circle-size: 16px;
+
+        $width: $circle-size * 2 + ($height - $circle-size);
+
+        position: relative;
+        display: inline-block;
+        width: $width;
+        height: $height;
+
+        input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+
+            &:checked + .slider {
+                background-color: #2195f3b3;
+            }
+
+            &:focus + .slider {
+                box-shadow: 0 0 1px #2195f3b3;
+            }
+
+            &:checked + .slider:before {
+                transform: translateX($circle-size - 1px);
+            }
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            -webkit-transition: 0.4s;
+            transition: 0.4s;
+
+            background-color: rgba($color: #fff, $alpha: 0.1);
+            border: rgba($color: #fff, $alpha: 0.1) 1px solid;
+
+            &.round {
+                border-radius: $width;
+                &:before {
+                    border-radius: 50%;
+                }
+            }
+
+            &:before {
+                position: absolute;
+                content: "";
+                height: $circle-size;
+                width: $circle-size;
+                left: ($height - $circle-size) / 2;
+                bottom: ($height - $circle-size) / 2 - 1px;
+                background-color: white;
+                -webkit-transition: 0.4s;
+                transition: 0.4s;
+            }
         }
     }
 </style>
